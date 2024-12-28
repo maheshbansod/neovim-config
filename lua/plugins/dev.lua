7561e100ab04e12a34f06d71b196def5ed862c9b
Mahesh Bansod <coolkevin011@gmail.com>
2024-12-28 20:56:41 +0530

    ai: get api key in setup

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -1,6 +1,16 @@
 return {
   {
     dir = "~/projects/ai.nvim",
+    config = function()
+      require('ai').setup({
+        api_key_getter = function()
+          -- get it from a file
+          local path = vim.fn.stdpath('data') .. "/secrets/google_ai_api_key"
+          local lines = vim.fn.readfile(path)
+          return lines[1]
+        end
+      })
+    end
   },
   {
     dir = "~/projects/present.nvim",
